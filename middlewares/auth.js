8b4926b5ae66ae550453d5fae5deab3ac8da0324
Ashish-Singh07 <itstheashish@gmail.com>
2022-10-17 22:01:11 +0530

    Authorizing users after validating jwt token and sending back name and email as response

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,15 @@
 var jwt = require("jsonwebtoken");
+var createError = require("http-errors");
 
 module.exports = {
   verifyToken: async (req, res, next) => {// this middleware is applied to the routes which will implement authorization using jwt
     console.log(req.headers);
-    var token = req.headers.authorization;
+    var auth = req.headers.authorization.split(' ');
+    var token = auth[1] || auth[0];
+
     try {
       if (!token) {
-        return next(createError(400,'Token Required'));
+        return next(createError(403,'Token Required'));
       }
       var payload = await jwt.verify(token, process.env.TOKEN_SECRET);
       req.user = payload; //all the routes after this will have access to the user which is logged in
